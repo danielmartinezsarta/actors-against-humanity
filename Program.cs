2796eb224537e2e7a4301e45f8c49d6071a2734d
Daniel Martinez <daniel@dmsa.com.py>
2022-10-31 14:41:57 -0300

    Akka dependency installed, player actor class created and simple console menu added.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Akka;
+using ActorsAgainstHumanity.Actors;
+using Akka.Actor;
 
-Console.WriteLine("Hello, World!");
+var actorSystem = ActorSystem.Create("aah-actorsystem");
+
+var playerActor = actorSystem.ActorOf<PlayerActor>("player-actor");
+
+var inputKey = ConsoleKey.Enter;
+while (inputKey != ConsoleKey.X)
+{
+    Console.WriteLine("\n");
+    Console.WriteLine("Select an option:");
+    Console.WriteLine("A: Create new game.");
+    Console.WriteLine("B: Join existing game.");
+    Console.WriteLine("X: Exit application.");
+
+    inputKey = Console.ReadKey().Key;
+}
+
+actorSystem.Terminate();
+
+
